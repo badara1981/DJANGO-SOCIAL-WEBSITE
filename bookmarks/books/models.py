a2b0c67dfe21392240cb6c4d2cfac07c756e09b5
Borko <bstoilov81@gmail.com>
2021-04-19 20:50:18 +0300

    Customize manager and add some methods in models

--- bookmarks/books/models.py
+++ bookmarks/books/models.py
@@ -15,12 +15,18 @@ class Country(models.Model):
         return self.name
 
 
+class PublisherManager(models.Manager):
+    def country_count(self, keywords):
+        return self.filter(country__name__icontains=keywords).count()
+
+
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='cityes')
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='countrys')
     website = models.URLField()
+    objects = PublisherManager()
 
     def __str__(self) -> str:
         return self.name
@@ -34,12 +40,24 @@ class Author(models.Model):
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
+    @property
+    def get_full_name(self):
+        return f"Authors: {self.first_name} {self.last_name}"
+
+
+
+class BookManager(models.Manager):
+    def title_count(self, keyword):
+        return self.filter(title__icontains=keyword).count()
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author, related_name='authors')
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     publication_date = models.DateField()
+    objects = BookManager()
+
 
     def __str__(self) -> str:
         return self.title
