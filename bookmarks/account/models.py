2decf6aed0ef14a6621b548d55dc30238f700349
Borko <bstoilov81@gmail.com>
2021-04-21 19:46:02 +0300

    User Profile add, models, form and view

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%M/%d', blank=True)
+
+    def __str__(self) -> str:
+        return 'Profile for user {}'.format(self.user.username)
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . views import user_login, dashboard, register
+from . views import user_login, dashboard, register, edit
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
+    path('edit/', edit, name='edit'),
     path('', dashboard, name='dashboard'),
     path('passwrord_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
