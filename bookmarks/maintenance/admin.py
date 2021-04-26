edbb00509ea015a204c19eb312d2533c7d659180
Borko <bstoilov81@gmail.com>
2021-04-26 20:20:28 +0300

    Models for maintenance

---
+++ bookmarks/maintenance/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Rooms, Problem
+
+admin.site.register(Rooms)
+admin.site.register(Problem)
---
+++ bookmarks/maintenance/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+import datetime
+
+class Rooms(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Problem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    room_name = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    solve = models.BooleanField(default=False)
+    date_publish = models.DateTimeField(default=datetime.datetime.now())
+    date_solve = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.message[:10]
+
